75563ee812310cab44d9573c77b1b1cb232fc4cb
Shayne Meyer <shayne.meyer@gmail.com>
2024-09-29 15:05:58 -0700

    initial setup

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>SupportMe</h1>
+      <Button>Log in</Button>
+      <Button>Sign up</Button>
+    </div>
+  );
+}
+export default LandingPage;
