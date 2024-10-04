199400d85ea4fafe9bd45d260c1ec4116fdd5ae2
Shayne Meyer <shayne.meyer@gmail.com>
2024-10-04 13:46:03 -0700

    added images for avatars

--- components/stats/EmployeeStats.tsx
+++ components/stats/EmployeeStats.tsx
@@ -1,19 +1,36 @@
-import { UserIcon } from "lucide-react";
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import {
+  AlertTriangleIcon,
+  BadgeCheckIcon,
+  UserCheck2Icon,
+  UserIcon,
+  UserRoundXIcon,
+} from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { Avatar, AvatarFallback } from "../ui/avatar";
 
 function EmployeeStats() {
+  const totalEmployees = 100;
+  const employeesPresent = 80;
+  const employeesPresentPercentage = (employeesPresent / totalEmployees) * 100;
+
   return (
     <div className="grid lg:grid-cols-3 gap-4">
       <Card>
-        <CardHeader>
+        <CardHeader className="pb-2">
           <CardTitle className="text-base">Total employees</CardTitle>
         </CardHeader>
         <CardContent className="flex justify-between items-center">
           <div className="flex gap-2">
             <UserIcon />
-            <div className="text-5xl font-bold">100</div>
+            <div className="text-5xl font-bold">{totalEmployees}</div>
           </div>
           <div>
             <Button size="xs" asChild>
@@ -23,14 +40,42 @@ function EmployeeStats() {
         </CardContent>
       </Card>
       <Card>
-        <CardHeader>
+        <CardHeader className="pb-2">
           <CardTitle className="text-base">Employees present</CardTitle>
         </CardHeader>
+        <CardContent>
+          <div className="flex gap-2">
+            {employeesPresentPercentage > 75 ? (
+              <UserCheck2Icon />
+            ) : (
+              <UserRoundXIcon />
+            )}
+            <div className="text-5xl font-bold">{employeesPresent}</div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          {employeesPresentPercentage > 75 ? (
+            <span className="text-xs text-green-500 flex gap-1 items-center">
+              <BadgeCheckIcon />
+              {employeesPresentPercentage}% of employees are present
+            </span>
+          ) : (
+            <span className="text-xs text-red-500 flex gap-1 items-center">
+              <AlertTriangleIcon />
+              Only {employeesPresentPercentage}% of employees are present
+            </span>
+          )}
+        </CardFooter>
       </Card>
       <Card className="border-pink-500">
-        <CardHeader>
+        <CardHeader className="pb-2">
           <CardTitle className="text-base">Employee of the month</CardTitle>
         </CardHeader>
+        <CardContent className="flex gap-2 items-center">
+          <Avatar>
+            <AvatarFallback>CM</AvatarFallback>
+          </Avatar>
+        </CardContent>
       </Card>
     </div>
   );
