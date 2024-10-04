16c9c0ad103c1f02ca58b0e690e5af06374a09f7
Shayne Meyer <shayne.meyer@gmail.com>
2024-10-04 08:01:38 -0700

    added dashboard tabs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,15 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <Tabs defaultValue="employees">
+      <TabsList>
+        <TabsTrigger value="employees">Employees stats</TabsTrigger>
+        <TabsTrigger value="teams">Team stats</TabsTrigger>
+      </TabsList>
+      <TabsContent value="employees">employees stats view</TabsContent>
+      <TabsContent value="teams">teams stats view</TabsContent>
+    </Tabs>
+  );
 }
 export default DashboardPage;
